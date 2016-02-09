6a49558883e644f0b8f165f9e1ec71fed7ba9b4a
GROSS Anthony <anthony.k.gross@gmail.com>
2016-02-09 13:12:31 +0100

    filtre antibot

--- src/src/Anthonykgrossfr/MainBundle/Controller/DefaultController.php
+++ src/src/Anthonykgrossfr/MainBundle/Controller/DefaultController.php
@@ -3,9 +3,18 @@
 namespace Anthonykgrossfr\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
+    static $questions = array(
+        array("trois plus un est égale à "      => array("4", "quatre")),
+        array("huit moins trois est égale à "   => array("5", "cinq")),
+        array("trois moins un est égale à " => array("2", "deux")),
+        array("zéro plus un est égale à " => array("1", "un")),
+        array("trois plus zéro est égale à " => array("3", "trois"))
+    );
+    
     public function indexAction()
     {
         return $this->render('AnthonykgrossfrMainBundle:Default:index.html.twig');
@@ -24,16 +33,37 @@ class DefaultController extends Controller
         $e          = array('msg' => array());
         $request    = $this->container->get('request');
         
-        $subject    = $request->get('subject', 'Aucun sujet');
-        $email      = trim($request->get('email', 'anthony.k.gross@gmail.com'));
-        $name       = trim($request->get('name', 'Inconnu'));
-        $msg        = $request->get('message', 'Aucun sujet');
+        $subject    = $request->get('subject',  null);
+        $email      = trim($request->get('email', null));
+        $name       = trim($request->get('name', null));
+        $msg        = $request->get('message', null);
+        $captcha_id = $request->get('captcha_id', null);
+        $captcha    = $request->get('captcha', null);
         
-        if(strlen($email)==0){
-            $e['msg'][] = " Sans email, je ne pourrais jamais vous répondre .. ";
+        if(is_null($subject) || strlen($subject)==0){
+            $e['msg'][] = "Un petit effort, précisez moi le sujet de votre message";
         }
-        if(strlen($name)==0){
+        if(is_null($msg) || strlen($msg)==0){
+            $e['msg'][] = "Y a-t-il une raison de m'envoyer un message vide ?";
+        }
+        if(is_null($email) || strlen($email)==0){
+            $e['msg'][] = "Sans email, je ne pourrais jamais vous répondre .. ";
+        }
+        if(is_null($name) || strlen($name)==0){
             $e['msg'][] = "Pouvez vous me renseigner au moins votre prénom s'il vous plait ? ";
+        }
+        if(is_null($captcha) || strlen($captcha)==0){
+            $e['msg'][] = "Je vous prierais de confirmer que vous n'êtes pas un bot (captcha).";
+        }
+        if(is_null($captcha_id) || strlen($captcha_id)==0){
+            $e['msg'][] = "Impossible de savoir si vous utilisez le bon captcha.";
+        }
+        if(!is_null($captcha) && !is_null($captcha_id)){
+            $response =  array_values(self::$questions[$captcha_id]);
+            
+            if(!in_array($captcha, $response[0])){
+                $e['msg'][] = "Votre réponse au captcha ne correspond pas à ce que je m'attendais.";
+            }
         }
         
         if(count($e['msg'])==0){
@@ -66,17 +96,32 @@ class DefaultController extends Controller
                 ;
                 $this->get('mailer')->send($message);
                 $e          = array('msg' => array("J'ai bien reçu votre message. Merci beaucoup !"));
+                $view       = new \Symfony\Component\HttpFoundation\JsonResponse($e, 200);
             }
             catch(\Exception $e){
                 $e          = array('msg' => array("Votre email n'est pas envoyé. Tenteriez-vous de me renseigner de fausses informations ? =) "));
+                $view       = new \Symfony\Component\HttpFoundation\JsonResponse($e, 500);
             }
+            return $view;
         }
-        $view       = new \Symfony\Component\HttpFoundation\JsonResponse($e);
+        $view       = new \Symfony\Component\HttpFoundation\JsonResponse($e, 500);
         return $view;
     }
     
     public function cvAction()
     {
         return $this->render('AnthonykgrossfrMainBundle:Default:cv.html.twig');
+    }
+    
+    public function captchaAction(){
+        $request    = $this->container->get('request');
+        if ($request->isXmlHttpRequest()) {
+            $idx      = rand(0, count(self::$questions)-1);
+            $response = new JsonResponse();
+            return $response->setData(array(
+                'idx'       => $idx,
+                'question'  => array_keys(self::$questions[$idx])[0]
+            ));
+        }
     }
 }
--- src/web/js/main.js
+++ src/web/js/main.js
@@ -226,17 +226,19 @@ jQuery(document).ready(function($){
 	/* Contact us process */
 	$("#contact-form").submit(function() {
 		var submitData	= $(this).serialize();
-		var $name		= $(this).find("input[name='name']");
-		var $email		= $(this).find("input[name='email']");
+		var $name	= $(this).find("input[name='name']");
+		var $email	= $(this).find("input[name='email']");
 		var $subject	= $(this).find("input[name='subject']");
 		var $message	= $(this).find("textarea[name='message']");
 		var $datastatus	= $(this).next('.data-status');
-		var $submit		= $(this).find("input[name='submit']");
-		
+		var $submit	= $(this).find("input[name='submit']");
+		var $captcha	= $(this).find("input[name='captcha']");
+                
 		$name.attr('disabled','disabled');
 		$email.attr('disabled','disabled');
 		$subject.attr('disabled','disabled');
 		$message.attr('disabled','disabled');
+                $captcha.attr('disabled','disabled');
 		$datastatus.show().html('<div class="alert alert-info"><strong>Loading...</strong></div>');
 		
 		$.ajax({ // Send an offer process with AJAX
@@ -249,6 +251,7 @@ jQuery(document).ready(function($){
                             $email.val('').removeAttr('disabled');
                             $subject.val('').removeAttr('disabled');
                             $message.val('').removeAttr('disabled');
+                            $captcha.val('').removeAttr('disabled','disabled');
                             $submit.removeAttr('disabled');
                             $datastatus.html('<div class="alert alert-info"><ul></ul></div>');
                             
@@ -256,14 +259,16 @@ jQuery(document).ready(function($){
                                 $datastatus.find("ul").append("<li><b>"+v+"</b></li>");
                             });
 			},
-                        error: function(data){
+                        error: function(d){
                             $name.removeAttr('disabled');
                             $email.removeAttr('disabled');
                             $subject.removeAttr('disabled');
                             $message.removeAttr('disabled');
                             $submit.removeAttr('disabled');
+                            $captcha.removeAttr('disabled');
                             $datastatus.html('<div class="alert alert-danger"><ul></ul></div>');
                             
+                            var data = JSON.parse(d.responseText);
                             $.each(data.msg, function(k, v){
                                 $datastatus.find("ul").append("<li><b>"+v+"</b></li>");
                             });
